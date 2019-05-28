4fb098838d699b6a86533a886e75c7384bbf37a4
billsioros <billsioros97@gmail.com>
2019-05-28 17:33:01 +0300

    Internal to Unrecoverable Error

--- semantic/detail/Context.java
+++ semantic/detail/Context.java
@@ -7,33 +7,33 @@ public interface Context
 {
     public String getIdentifier();
 
-    default public Variable acquireVariable(String identifier) throws Exception, InternalError
+    default public Variable acquireVariable(String identifier) throws Exception, UnrecoverableError
     {
-        throw new InternalError("No implementation of the 'acquireVariable' method");
+        throw new UnrecoverableError("No implementation of the 'acquireVariable' method");
     }
 
-    default public void register(Variable variable) throws Exception, InternalError
+    default public void register(Variable variable) throws Exception, UnrecoverableError
     {
-        throw new InternalError("No implementation of the 'register(Variable)' method");
+        throw new UnrecoverableError("No implementation of the 'register(Variable)' method");
     }
 
-    default public Function acquireFunction(String identifier) throws Exception, InternalError
+    default public Function acquireFunction(String identifier) throws Exception, UnrecoverableError
     {
-        throw new InternalError("No implementation of the 'acquireFunction' method");
+        throw new UnrecoverableError("No implementation of the 'acquireFunction' method");
     }
 
-    default public void register(Function function) throws Exception, InternalError
+    default public void register(Function function) throws Exception, UnrecoverableError
     {
-        throw new InternalError("No implementation of the 'register(Function)' method");
+        throw new UnrecoverableError("No implementation of the 'register(Function)' method");
     }
 
-    default public Base acquireClass(String identifier) throws Exception, InternalError
+    default public Base acquireClass(String identifier) throws Exception, UnrecoverableError
     {
-        throw new InternalError("No implementation of the 'acquireClass' method");
+        throw new UnrecoverableError("No implementation of the 'acquireClass' method");
     }
 
-    default public void register(Base base) throws Exception, InternalError
+    default public void register(Base base) throws Exception, UnrecoverableError
     {
-        throw new InternalError("No implementation of the 'register(Base)' method");
+        throw new UnrecoverableError("No implementation of the 'register(Base)' method");
     }
 }
