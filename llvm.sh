678d84b6e7fbfab49828f0e0ec287316109a6f71
billsioros <billsioros97@gmail.com>
2019-06-02 16:45:30 +0300

    llvm opens *.llvm

--- llvm.sh
+++ llvm.sh
@@ -3,7 +3,7 @@
 ./compile.sh --clean
 ./compile.sh
 java Main ./examples/positive/BinaryTree.mini > ./structure
-code ./examples/positive/BinaryTree.mini ./structure ./examples/positive/BinaryTree.ll
+code ./examples/positive/BinaryTree.mini ./structure ./examples/positive/BinaryTree.ll ./examples/llvm/BinaryTree.llvm
 clang -o binary ./examples/positive/BinaryTree.ll
 
 ./binary # > ./debug
